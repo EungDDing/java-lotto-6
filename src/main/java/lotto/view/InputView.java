4e2ff60cee63aaecd7df4d35c0368f8f8da02c5b
LEM <ppoppo9802@naver.com>
2023-11-05 04:31:57 +0900

    feat(InputView): 사용자에게 입력 부분을 알려주기 위한 메세지들 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,8 +1,19 @@
 package lotto.view;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
+    public void printInputMoney() {
+        System.out.println("구입 금액을 입력해 주세요.");
+    }
+    public void printInputWinngNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+    public void printInputBonusNumber() {
+        System.out.println("보너스 번호를 입혁해 주세요.");
+    }
     public String inputWinningNumber() {
         String winningNumber = readLine();
         return winningNumber;
