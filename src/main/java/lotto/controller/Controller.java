9ee4b59e0b4c21eb01ce7409a42389fb67d913fc
LEM <ppoppo9802@naver.com>
2023-11-05 05:54:28 +0900

    feat(Controller): 사용자로부터 당첨 번호를 입력받는 기능 추가

--- src/main/java/lotto/controller/Controller.java
+++ src/main/java/lotto/controller/Controller.java
@@ -1,13 +1,41 @@
 package lotto.controller;
 
+import lotto.domain.Lotto;
+import lotto.view.InputView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Controller {
-    public void run() {
+    private static final String SLICING_SYMBOL = ",";
 
+    public void run() {
+        try {
+            getWinningNumber();
+        } catch (IllegalArgumentException e) {
+            getWinningNumber();
+        }
     }
     public String inputWinningNumber() {
         String winningNumber = readLine();
         return winningNumber;
+    }
+    public List<Integer> convertStringToIntegerList(String winnigNumber){
+        List<Integer> winningNumbers = Arrays.stream(winnigNumber.split(SLICING_SYMBOL)).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+        return winningNumbers;
+    }
+    public Lotto saveWinningnumber() {
+        String winningNumber = inputWinningNumber();
+        List<Integer> winningNumbers = convertStringToIntegerList(winningNumber);
+        Lotto lotto = new Lotto(winningNumbers);
+        return lotto;
+    }
+    public void getWinningNumber() {
+        InputView.printInputWinngNumber();
+        Lotto lotto = saveWinningnumber();
     }
 }
