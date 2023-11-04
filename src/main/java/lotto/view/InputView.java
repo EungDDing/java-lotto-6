56a70ee90f736afc6db1f262397ceea1015a0ad2
LEM <ppoppo9802@naver.com>
2023-11-05 05:42:34 +0900

    refactor(InputView): 클래스 내의 함수를 static화

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,13 +5,13 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public void printInputMoney() {
+    public static void printInputMoney() {
         System.out.println("구입 금액을 입력해 주세요.");
     }
-    public void printInputWinngNumber() {
+    public static void printInputWinngNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
     }
-    public void printInputBonusNumber() {
+    public static void printInputBonusNumber() {
         System.out.println("보너스 번호를 입혁해 주세요.");
     }
 }
