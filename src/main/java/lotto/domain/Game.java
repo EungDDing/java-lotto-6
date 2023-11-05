c49b39dfed8aa8a123dba28b09430862cdcda4f9
LEM <ppoppo9802@naver.com>
2023-11-06 01:08:47 +0900

    refactor(Game): Game class 안에 변수 변경

--- src/main/java/lotto/domain/Game.java
+++ src/main/java/lotto/domain/Game.java
@@ -1,19 +1,21 @@
 package lotto.domain;
 
+import java.util.List;
+
 public class Game {
-    private final Lotto winningNumber;
-    private final BonusNumber bonusNumber;
+    private final List<Integer> winningNumber;
+    private final int bonusNumber;
 
     public Game(Lotto winningNumber, BonusNumber bonusNumber) {
-        this.winningNumber = winningNumber;
-        this.bonusNumber = bonusNumber;
+        this.winningNumber = winningNumber.getNumbers();
+        this.bonusNumber = bonusNumber.getBonusNumber();
     }
 
-    public Lotto getWinningNumber() {
+    public List<Integer> getWinningNumber() {
         return winningNumber;
     }
 
-    public BonusNumber getBonusNumber() {
+    public int getBonusNumber() {
         return bonusNumber;
     }
 }
