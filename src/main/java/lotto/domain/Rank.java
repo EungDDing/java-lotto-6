2fcc6f2e92fcf4d6c35a789774eece1aeabd82d4
LEM <ppoppo9802@naver.com>
2023-11-05 20:37:42 +0900

    style(Rank): Rank enum class의 직관성을 위해 result를 숫자로만 변경

--- src/main/java/lotto/domain/Rank.java
+++ src/main/java/lotto/domain/Rank.java
@@ -1,23 +1,23 @@
 package lotto.domain;
 
 public enum Rank {
-    FIRST(6, 2000000000, "6개 일치 (2,000,000,000원)"),
-    SECOND(5, 30000000, "5개 일치, 보너스 볼 일치 (30,000,000원)"),
-    THIRD(5, 1500000, "5개 일치 (1,500,000원)"),
-    FOURTH(4, 50000, "4개 일치 (50,000원)"),
-    FIFTH(3, 5000, "3개 일치 (5,000원)"),
-    FAIL_MATCH_TWO(2, 0, "2개 일치 (0원)"),
-    FAIL_MATCH_ONE(1, 0, "1개 일치 (0원)"),
-    FAIL_MATCH_ZERO(0, 0, "0개 일치 (0원)");
+    FIRST(6, 2000000000, "2,000,000,000"),
+    SECOND(5, 30000000, "30,000,000"),
+    THIRD(5, 1500000, "1,500,000"),
+    FOURTH(4, 50000, "50,000"),
+    FIFTH(3, 5000, "5,000"),
+    FAIL_MATCH_TWO(2, 0, "0"),
+    FAIL_MATCH_ONE(1, 0, "0"),
+    FAIL_MATCH_ZERO(0, 0, "0");
 
     private final int match;
-    private final int prize;
+    private final long prize;
     private final String result;
 
-    Rank(int match, int prize, String result) {
+    Rank(int match, long prize, String result) {
         this.match = match;
         this.prize = prize;
         this.result = result;
     }
-    
+
 }
