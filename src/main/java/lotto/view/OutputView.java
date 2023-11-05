032b8a7f519e6bffa536ae8840024ace5577414b
LEM <ppoppo9802@naver.com>
2023-11-06 03:00:24 +0900

    feat: 로또의 구입 개수를 출력하는 함수 추가

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,12 @@
+package lotto.view;
+
+import lotto.domain.Lotto;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printNumberOfGames(int number) {
+        System.out.println();
+        System.out.println(number + "개를 구입했습니다.");
+    }
+}
