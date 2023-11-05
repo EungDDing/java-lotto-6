c883fe37b389b40489e745cb11cb92e87951217d
LEM <ppoppo9802@naver.com>
2023-11-06 01:42:10 +0900

    feat(Service): 로또 번호를 자동으로 6개 뽑아주는 함수 추가

--- src/main/java/lotto/service/Service.java
+++ src/main/java/lotto/service/Service.java
@@ -1,11 +1,16 @@
 package lotto.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import lotto.domain.Game;
+import lotto.domain.Lotto;
 import lotto.domain.Money;
 
 import java.util.List;
 
 public class Service {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int SIZE_OF_LOTTO = 6;
     public int numberOfGame(Money buy) {
         return buy.numberOfLotto();
     }
@@ -14,5 +19,10 @@ public class Service {
     }
     public int bonusNumber(Game game) {
         return game.getBonusNumber();
+    }
+    public Lotto generateRandomLottoNumbers() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, SIZE_OF_LOTTO);
+        Lotto lotto = new Lotto(randomNumbers);
+        return lotto;
     }
 }
