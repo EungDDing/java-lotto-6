8b3519f3774fb7036c141f393f60ec4a8b4ab920
LEM <ppoppo9802@naver.com>
2023-11-06 03:35:01 +0900

    refactor(Service): List<Lotto> lottos를 전역변수로 설정

--- src/main/java/lotto/service/Service.java
+++ src/main/java/lotto/service/Service.java
@@ -5,24 +5,35 @@ import lotto.domain.Game;
 import lotto.domain.Lotto;
 import lotto.domain.Money;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Service {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
     private static final int SIZE_OF_LOTTO = 6;
-    public int numberOfGame(Money buy) {
+
+    private static List<Lotto> lottos;
+
+    public static int numberOfGame(Money buy) {
         return buy.numberOfLotto();
     }
-    public List<Integer> winningNumber(Game game) {
+    public static List<Integer> winningNumber(Game game) {
         return game.getWinningNumber();
     }
-    public int bonusNumber(Game game) {
+    public static int bonusNumber(Game game) {
         return game.getBonusNumber();
     }
-    public Lotto generateRandomLottoNumbers() {
+    public static Lotto generateRandomLottoNumbers() {
         List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, SIZE_OF_LOTTO);
         Lotto lotto = new Lotto(randomNumbers);
         return lotto;
+    }
+    public static List<Lotto> randomLottos(Money buy) {
+        lottos = new ArrayList<>();
+        for (int i = 0; i < numberOfGame(buy); i++) {
+            lottos.add(generateRandomLottoNumbers());
+        }
+        return lottos;
     }
 }
