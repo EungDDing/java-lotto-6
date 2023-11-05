747766ee0f5a2dae88255bbdaf7959174c457042
LEM <ppoppo9802@naver.com>
2023-11-06 03:38:25 +0900

    style(InputView): 출력 결과에 맞게 한 줄 띄기 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -5,9 +5,11 @@ public class InputView {
         System.out.println("구입 금액을 입력해 주세요.");
     }
     public static void printInputWinngNumber() {
+        System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
     }
     public static void printInputBonusNumber() {
+        System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
     }
 }
