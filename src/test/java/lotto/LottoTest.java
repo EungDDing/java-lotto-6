4f5712403bbe1717f903b3da3740539a5dbf0a10
LEM <ppoppo9802@naver.com>
2023-11-06 01:52:34 +0900

    test(LottoTest): 로또 리스트가 제대로 정렬되는지 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
@@ -31,5 +32,11 @@ class LottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(() -> new Lotto(List.of(1, 1, 2, 3, 4, 60)))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+    @DisplayName("로또 번호가 제대로 정렬이 되는지 확인하는 테스트")
+    @Test
+    void checkSortNumbers() {
+        Lotto lotto = new Lotto(List.of(3, 1, 2, 4, 6, 5));
+        assertThat(lotto.getNumbers()).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
     }
 }
